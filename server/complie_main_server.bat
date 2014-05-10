7a63a955e4ace07b47997096b1cff83efb1adf3c
heimonsy <heimonsy@gmail.com>
2014-05-10 11:51:23 +0000

    updonw bat perfect

---
+++ server/complie_main_server.bat
@@ -0,0 +1,3 @@
+javac -Xlint:unchecked -Xlint:deprecation -encoding "utf-8" -classpath "%CLASSPATH%;./lib;./lib/log4j-1.2.17.jar;./lib/mongo-java-driver-2.12.1.jar;./lib/commons-cli-1.1.jar;./lib/commons-io-1.4.jar;./lib/jodconverter-core-3.0-beta-4.jar;./lib/json-20090211.jar;./lib/juh-3.2.1.jar;./lib/jurt-3.2.1.jar;./lib/ridl-3.2.1.jar;./lib/unoil-3.2.1.jar" MainServer.java
+
+pause
