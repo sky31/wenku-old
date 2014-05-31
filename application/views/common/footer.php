db98aea065303ada80ec649125ea6c302cda1da9
heimonsy <heimonsy@gmail.com>
2014-05-31 04:35:07 +0000

    除了删除文件功能，其余基本完成

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -35,6 +35,6 @@
 		<script src="/static/js/jquery-1.10.2.min.js"></script>
 		<script src="/static/js/bootstrap.min.js"></script>
 		<script src="/static/js/jquery.uploadify.min.js"></script>
-		<script src="/static/js/doc.js?v=4"></script>
+		<script src="/static/js/doc.js?v=5"></script>
 	</body>
 </html>
